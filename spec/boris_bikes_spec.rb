2a775b84c7075428b1d46492e30a0d7c36f5974d
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-04-16 17:18:39 +0100

    Raise exception when no bike is available

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,12 +1,18 @@
 require 'boris_bikes'
 
-describe DockingStation.new do
+describe DockingStation do
 
-  it { is_expected.to respond_to(:release_bike) }
+  it "responds to release_bike method" do
+    expect(subject).to respond_to(:release_bike)
+  end
 
-  it { is_expected.to respond_to(:dock).with(1).argument }
+  it "responds to dock method and takes 1 argument" do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
 
-  it { is_expected.to respond_to(:bike) }
+  it "responds to bike method" do
+    expect(subject).to respond_to(:bike)
+  end
 
   it "docks a bike" do
     bike = Bike.new
@@ -17,6 +23,19 @@ describe DockingStation.new do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
+  end
+
+  describe '#release_bike' do
+
+    it "releases a bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'throws an error when no bike available' do
+      expect { subject.release_bike }.to raise_error "No bikes available"
+    end
   end
 
 end
---
+++ spec/feature_spec.rb
@@ -0,0 +1,5 @@
+require './lib/boris_bikes'
+
+station = DockingStation.new
+
+station.release_bike
