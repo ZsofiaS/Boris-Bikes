3d1e0f50ae3988bf2978bf551ad32997410bd721
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-04-16 15:40:41 +0100

    Add dock method and instance variable

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,6 +1,22 @@
 require 'boris_bikes'
 
 describe DockingStation.new do
-  it { is_expected.to respond_to(:release_bike)}
+
+  it { is_expected.to respond_to(:release_bike) }
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) }
+
+  it "docks a bike" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it "shows returned bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 
 end
