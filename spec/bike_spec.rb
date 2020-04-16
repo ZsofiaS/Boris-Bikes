0337872e71970ab8d8b13fa86af9cd6c0d556003
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-04-16 14:31:46 +0100

    Add test for working method

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,7 +1,14 @@
 require "boris_bikes"
 
 describe Bike do
+
   it "checks if it's working" do
     expect(Bike.new).to respond_to(:working?)
+  end
+
+  describe ".working?" do
+    it "checks if bike is working" do
+      expect(subject.working?).to be(true)
+    end
   end
 end
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -2,4 +2,5 @@ require 'boris_bikes'
 
 describe DockingStation.new do
   it { is_expected.to respond_to(:release_bike)}
+
 end
